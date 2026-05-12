2a3e0993e02a73bed488948b4cc7da7c8885f006
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-05-12 15:23:34 -0300

    Create BlueprintSection.tsx

---
+++ components/home/BlueprintSection.tsx
@@ -0,0 +1,13 @@
+export default function BlueprintSection() {
+  return (
+    <section className="blueprint-section">
+
+      <img
+        src="/planta.png"
+        alt="Planta"
+        className="blueprint-image"
+      />
+
+    </section>
+  );
+}
