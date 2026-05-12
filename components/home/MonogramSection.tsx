0b3e49817e2a500a1663c1d4e29e16850491461d
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-05-12 15:24:02 -0300

    Create MonogramSection.tsx

---
+++ components/home/MonogramSection.tsx
@@ -0,0 +1,13 @@
+export default function MonogramSection() {
+  return (
+    <section className="monogram-section">
+
+      <img
+        src="/monograma-background.png"
+        alt="Monograma"
+        className="monogram-image"
+      />
+
+    </section>
+  );
+}
