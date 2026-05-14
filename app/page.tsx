205d444c81e55310841bd65d28ed7c885c375662
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-05-14 18:52:51 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "@/components/home/HomePage";
+import LuxuryHome from "@/components/LuxuryHome";
 
-export default function Page() {
-  return <HomePage />;
+export default function Home() {
+  return <LuxuryHome />;
 }
