dd12ae0f0f7204a187e4ed206e2b80eaa002c219
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-05-14 18:43:37 -0300

    Update HomePage.tsx

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -1,21 +1,9 @@
-import HeroSection from "./HeroSection";
-import BlueprintSection from "./BlueprintSection";
-import MonogramSection from "./MonogramSection";
-import NumbersSection from "./NumbersSection";
-import ProjectsSection from "./ProjectsSection";
-import SaoPauloSection from "./SaoPauloSection";
-import FooterSection from "./FooterSection";
+import HomeSection from "./HomeSection";
 
 export default function HomePage() {
   return (
     <main>
-      <HeroSection />
-      <BlueprintSection />
-      <MonogramSection />
-      <NumbersSection />
-      <ProjectsSection />
-      <SaoPauloSection />
-      <FooterSection />
+      <HomeSection />
     </main>
   );
 }
